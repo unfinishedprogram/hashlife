166ad7c56c160beeca97f6d8f2f2575aae1ef655
Noah Labrecque <noah.labrecque@gmail.com>
2024-11-25 19:11:58 -0500

    compress cell_id

--- src/life/cell_id.rs
+++ src/life/cell_id.rs
@@ -1,25 +1,41 @@
 #[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
+#[repr(transparent)]
+struct CellIndex(usize);
+
+impl CellIndex {
+    pub fn new(layer: u16, index: usize) -> Self {
+        CellIndex(((layer as usize) << 48) | index)
+    }
+
+    pub fn index(&self) -> usize {
+        self.0 & 0x0000_ffff_ffff_ffff
+    }
+
+    pub fn layer(&self) -> usize {
+        (self.0 >> 48) & 0xffff
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 pub struct CellId {
-    layer: usize,
-    index: usize,
     alive: usize,
+    index: CellIndex,
 }
 
 impl CellId {
     pub fn new(layer: usize, index: usize, alive: usize) -> Self {
         CellId {
-            layer,
-            index,
+            index: CellIndex::new(layer as u16, index),
             alive,
         }
     }
 
     pub fn layer(&self) -> usize {
-        self.layer
+        self.index.layer()
     }
 
     pub fn index(&self) -> usize {
-        self.index
+        self.index.index()
     }
 
     pub fn alive(&self) -> usize {
