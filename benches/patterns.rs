cb2ca4d3d64e138cc6505b65625eb0ca55643a99
Noah Labrecque <noah.labrecque@gmail.com>
2025-03-22 14:20:28 -0400

    improve bench

--- benches/patterns.rs
+++ benches/patterns.rs
@@ -4,13 +4,8 @@ use hashlife::life::Life;
 pub fn clock_pattern(c: &mut Criterion) {
     let mut life = Life::from_rle(include_str!("../patterns/clock.rle"));
 
-    for _ in 0..1000 {
-        life.step();
-    }
-
-    c.bench_function("clock 1000-1001", |b| {
+    c.bench_function("clock", |b| {
         b.iter(|| {
-            let mut life = life.clone();
             life.step();
             black_box(life.root.alive())
         })
